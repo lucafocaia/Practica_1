014c29453ee354138e1143b37626b38aff75cd48
lucafocaia <lucafocaia.12@gmail.com>
2023-09-03 17:42:42 -0300

    3/9/23

--- Practica_1/Ejercicio4_P1.R
+++
@@ -1,8 +0,0 @@
-# Ejercicio 4 -------------------------------------------------------------
-
-### (a)
-print("Hola mundo")
-
-### (b)
-Número=readline("Ingrese un numero: ")
-print(paste(Número))
--- Practica_1/Ejercicio9_P1.R
+++
@@ -1,7 +0,0 @@
-# Ejercicio 9 -------------------------------------------------------------
-
-Nombre=readline("Ingrese su nombre: ")
-b=readline("Ingrese su edad: ")
-x=as.numeric(b)
-y=x+8
-print(paste(Nombre,"va a tener",y,"anios en 2030"))
